e412b1844c4ea61ecfdf3b2488bb19d9abf16f40
Sundar K <sundar_babu@hotmail.com>
2021-07-14 13:54:53 -0500

    i

--- security-groups.tf
+++ security-groups.tf
@@ -2,6 +2,7 @@
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id      = module.vpc.vpc_id
+  description = worker_group_mgmt_one
 
   ingress {
     from_port = 22
@@ -17,6 +18,7 @@ resource "aws_security_group" "worker_group_mgmt_one" {
 resource "aws_security_group" "worker_group_mgmt_two" {
   name_prefix = "worker_group_mgmt_two"
   vpc_id      = module.vpc.vpc_id
+  description = worker_group_mgmt_two
 
   ingress {
     from_port = 22
@@ -32,6 +34,7 @@ resource "aws_security_group" "worker_group_mgmt_two" {
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
   vpc_id      = module.vpc.vpc_id
+  description = all_worker_management
 
   ingress {
     from_port = 22
--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,16 @@
 variable "region" {
-  default     = "us-east-2"
+  default     = "us-east-1"
   description = "AWS region"
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "demo-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -22,7 +22,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.66.0"
 
-  name                 = "education-vpc"
+  name                 = "demo-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
